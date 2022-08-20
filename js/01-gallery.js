e8cc42558ed3a3f7b73922cd299a1ef36cd20682
Andrii Pozniak <andriipozniak@gmail.com>
2022-08-20 20:49:48 +0300

    task1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,34 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const imgGalleryCard = document.querySelector(`.gallery`);
+const imgGallery = onCreateImgCard(galleryItems);
 
-console.log(galleryItems);
+// Create card
+imgGalleryCard.insertAdjacentHTML("beforeend", imgGallery);
+
+function onCreateImgCard(galleryItems) {
+    return galleryItems.map(({preview, original, description}) => {
+       return `
+       <div>
+            <img class="gallery__image" 
+            src=${preview} 
+            original=${original}
+            alt=${description}/>
+       </div>` ;       
+    }).join("");
+    
+};
+// delegation
+imgGalleryCard.addEventListener(`click`, onImgCardClick);
+
+function onImgCardClick(evn) {
+   const isImgCard = evn.target.classList.contains(`gallery__image`);
+   if (!isImgCard) {
+    return;
+   }
+   const imgEl = evn.target
+   imgEl.src = imgEl.attributes.original.nodeValue;
+    console.log(evn)
+   console.log(imgEl.attributes.original.nodeValue)
+}
+// console.log(galleryItems);
