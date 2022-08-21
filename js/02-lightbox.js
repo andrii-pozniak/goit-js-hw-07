b5c7365e3a49ebfb476ce93507433481db26ddc2
Andrii Pozniak <andriipozniak@gmail.com>
2022-08-21 22:07:37 +0300

    add lightBox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,50 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const imgGalleryCard = document.querySelector(`.gallery`);
+const imgGallery = onCreateImgCard(galleryItems);
+
+// Create card
+imgGalleryCard.insertAdjacentHTML("beforeend", imgGallery);
+
+function onCreateImgCard(galleryItems) {
+    return galleryItems.map(({preview, original, description}) => {
+       return `
+       <div class="gallery__item">
+         <a class="gallery__link" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+            />
+       </a>
+      </div>
+       ` ;       
+    }).join("");
+    
+};
+// delegation
+imgGalleryCard.addEventListener(`click`, onImgCardClick);
+
+
+
+function onImgCardClick(evn) {
+   const isImgCard = evn.target.classList.contains('gallery__image');
+   evn.preventDefault()
+   if (!isImgCard) {
+    return;
+   }
+   const imgEl = document.querySelector(`.gallery__link`)
+   imgEl.href = evn.target.dataset.source;
+   
+   const instance = basicLightbox.create(`
+    <img class="modal__image" src="assets/images/image.png" width="800" height="600">
+   `)     
+   instance.show();
+   const modalEl = document.querySelector(`.modal__image`);
+   // console.log(basicLightbox_placeholder.src)
+      modalEl.src = evn.target.dataset.source;
+    console.log(modalEl.src)
+}
 console.log(galleryItems);
