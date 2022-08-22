3513a79dd075d1ba768c687af9a1aee377f37411
Andrii Pozniak <andriipozniak@gmail.com>
2022-08-22 23:04:27 +0300

    change lightBox

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -11,8 +11,7 @@ function onCreateImgCard(galleryItems) {
        return `
        <div class="gallery__item">
          <a class="gallery__link" href="${original}">
-            <img
-            class="gallery__image"
+            <img class="gallery__image"
             src="${preview}"
             data-source="${original}"
             alt="${description}"
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,5 +1,4 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
 const imgGalleryCard = document.querySelector(`.gallery`);
 const imgGallery = onCreateImgCard(galleryItems);
@@ -11,15 +10,14 @@ function onCreateImgCard(galleryItems) {
     return galleryItems.map(({preview, original, description}) => {
        return `
        <div class="gallery__item">
-         <a class="gallery__link" href="${original}">
-            <img
-            class="gallery__image"
-            src="${preview}"
-            
-            alt="${description}"
-            />
-       </a>
-      </div>
+        <a class="gallery__link" href="${original}">
+          <img class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+          />
+     </a>
+    </div>
        ` ;       
     }).join("");
     
@@ -27,30 +25,24 @@ function onCreateImgCard(galleryItems) {
 // delegation
 imgGalleryCard.addEventListener(`click`, onImgCardClick);
 
-
-
 function onImgCardClick(evn) {
-   const isImgCard = evn.target.classList.contains('gallery__image');
+    console.log("hello")
+const isImgCard = evn.target.classList.contains('gallery__image', {
+    
+});
    evn.preventDefault()
+   console.log(isImgCard)
    if (!isImgCard) {
     return;
    }
    const imgEl = document.querySelector(`.gallery__link`)
    imgEl.href = evn.target.dataset.source;
+
    
-// //    const instance = basicLightbox.create(`
-//     <img class="modal__image" src="assets/images/image.png" width="800" height="600">
-//    `)     
-//    instance.show();
+
    const modalEl = document.querySelector(`.modal__image`);
-   // console.log(basicLightbox_placeholder.src)
-    //   modalEl.src = evn.target.dataset.source;
-    // console.log(modalEl.src)
-    let lightbox = new simpleLightbox('.gallery a', { /* options */ });
-    // let lightbox = $('.gallery a').simpleLightbox({ /* options */ });
-    const instance = simpleLightbox.options
-//    instance.show();
-    console.log(simpleLightbox)
-    console.log(instance)
+   
+   console.log("OK")
+     new SimpleLightbox(`.gallery__item a`);
+    
 }
-console.log(galleryItems);
